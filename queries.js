67436cf6635a29e00ec5507429dc6c482d0c7c82
Ganiu Oluwaseto Akowanu <Akowanu-GO@ulster.ac.uk>
2023-12-15 12:50:22 +0000

    update syntax

--- queries.js
+++ queries.js
@@ -12,3 +12,18 @@ WHERE
   AND parameter = 'EV sale'
   AND powertrain = 'EV'
   AND country IN ('United Kingdom', 'United States', 'Uruguay', 'Zimbabwe', 'Ukraine', 'Turkey');
+
+
+  %jdbc(hive)
+INSERT OVERWRITE TABLE derived_ev_sales
+SELECT
+    CASE
+        WHEN country = 'USA' THEN 'United States'
+        ELSE country
+    END AS country,
+    parameter,
+    powertrain,
+    year,
+    value
+FROM
+  derived_ev_sales;
